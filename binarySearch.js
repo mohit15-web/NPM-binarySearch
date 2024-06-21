1db5c14255399e8975e210b91f9d704859d2de53
MohitChaudhari <mohitchoudharu2@gmail.com>
2024-06-21 17:39:25 +0530

    binarySearch

---
+++ binarySearch.js
@@ -0,0 +1,17 @@
+function binarySearch(arr,target){
+    let low = 0;
+    let high = arr.length - 1;
+    while(low <= high){
+        const mid = Math.floor((low + high) / 2)
+        if(arr[mid] === target){
+            return mid
+        }else if(arr[i] < target){
+            low = mid + 1
+        }else{
+            high = mid - 1
+        }
+    }
+    return -1
+}
+
+module.exports = binarySearch;
